fe2dc22c0847a2cc765828d58eeba0575fd81602
jackie <zhuweiping@madxstudio.cn>
2015-09-10 15:52:11 +0800

    add code

---
+++ app/src/main/jni/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+ 
+include $(CLEAR_VARS)
+ 
+LOCAL_MODULE    := tutorial03
+LOCAL_SRC_FILES := tutorial03.c
+LOCAL_LDLIBS := -llog -ljnigraphics -lz -landroid
+LOCAL_SHARED_LIBRARIES := libavformat libavcodec libswscale libavutil
+ 
+include $(BUILD_SHARED_LIBRARY)
+$(call import-module)
